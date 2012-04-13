7932cf743e106a212c0ed71bbc92d0c721ec569b
Mike Flynn <mfflynn@gmail.com>
2012-04-13 13:47:16 -0500

    Added main namespace to the Project file.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject ClojureSprites "1.0.0-SNAPSHOT"
   :description "A Clojure application to automatically create and implement a sprite from a CSS file."
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :main ClojureSprites.core)
