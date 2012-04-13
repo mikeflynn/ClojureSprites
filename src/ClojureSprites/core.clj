0dfe216f2f64b09f2967231d1dabf11faa8551bb
Mike Flynn <mfflynn@gmail.com>
2012-04-13 12:48:05 -0500

    Initial commit.

---
+++ src/ClojureSprites/core.clj
@@ -0,0 +1,1 @@
+(ns ClojureSprites.core)
---
+++ test/ClojureSprites/test/core.clj
@@ -0,0 +1,6 @@
+(ns ClojureSprites.test.core
+  (:use [ClojureSprites.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
